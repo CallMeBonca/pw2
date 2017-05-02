7759840390510cf843c7cdb61f2a874cdbd49684
CallMeBonca <adityareiza@gmail.com>
2017-05-02 13:16:17 +0700

    soaltadi

--- template-depan/footer.php
+++ template-depan/footer.php
@@ -5,3 +5,11 @@
                 </div>
             </div>
         </footer>
+        <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
